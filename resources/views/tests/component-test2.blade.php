e2e3a209825b884d58411d35869c20035bb7950a
kensuke takeshima <kensuke.takeshima0104@gmail.com>
2023-09-23 13:51:25 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,9 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー2</x-slot>
-コンポネントテスト2
+
+コンポ-ネントテスト2
+<x-test-class-base classBaseMessage="メッセージです" />
+<div class="mb-4"></div>
+<x-test-class-base classBaseMessage="メッセージです"  defaultMessage="初期値から変更"/>
+
 </x-tests.app>
